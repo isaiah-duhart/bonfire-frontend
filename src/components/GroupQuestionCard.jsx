15e053115ba436df333e0f735ff183b4727ef1c9
Isaiah Duhart <isaiah.duhart.1@gmail.com>
2025-11-01 19:52:01 -0400

    [4] Group questions are colored differently if user or other created the question

--- src/components/GroupQuestionCard.jsx
+++ src/components/GroupQuestionCard.jsx
@@ -1,5 +1,10 @@
 import { Link } from "@tanstack/react-router"
+import { useAuth } from "../context/AuthContext"
+
+import '../styles/groupQuestionsCard.css'
 
 export default function GroupQuestionCard({ groupQuestion }) {
-    return <Link to={`/group-questions/${groupQuestion.id}?text=${encodeURIComponent(groupQuestion.question_text)}`}>{groupQuestion.question_text}</Link>
+    const { userID } = useAuth()
+    const isOwnQuestion = userID === groupQuestion.created_by
+    return <Link className={isOwnQuestion ? 'own-question' : 'other-question'} to={`/group-questions/${groupQuestion.id}?text=${encodeURIComponent(groupQuestion.question_text)}`}>{groupQuestion.question_text}</Link>
 }
